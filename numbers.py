b0ddc212f2ae64e236de1290a23c688fd1143555
dron-ex <klin-008@mail.ru>
2024-12-17 23:14:10 +0300

    5 файлов

---
+++ numbers.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+ab= a + b
+cd = c + d
+abcd = ab/cd
+print(round(abcd, 2))
---
+++ user.py
@@ -0,0 +1,4 @@
+name = input("What is your name?")
+age = input("How old are you?")
+city = input("Where are you live?")
+print("Your name: {0} \nYour age: {1}  \nYou live in {2} ".format(name, age, city) )
